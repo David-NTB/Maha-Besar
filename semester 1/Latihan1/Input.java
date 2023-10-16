b412abcc40abdd9e9e583c44db0df88032ac2d89
David NTB <davidntb7000@gmail.com>
2023-10-16 18:05:37 +0700

    inisiasi latihan1

---
+++ semester 1/Latihan1/Input.java
@@ -0,0 +1,7 @@
+package Latihan1;
+
+public class Input {
+    public static void main(String[] args) {
+        System.out.println("Hellooww");
+    }
+}
---
+++ semester 1/Latihan1/SelisihWaktu.java
@@ -0,0 +1,7 @@
+package Latihan1;
+
+public class SelisihWaktu {
+    public static void main(String[] args) {
+        System.out.println("Helloasd");
+    }
+}
