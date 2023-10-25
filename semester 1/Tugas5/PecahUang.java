da9018255ca2c24142d942d666a540e8ceed79da
David NTB <davidntb7000@gmail.com>
2023-10-25 13:27:15 +0700

    tugas jurnal

---
+++ semester 1/Tugas5/PecahUang.java
@@ -0,0 +1,36 @@
+package Tugas5;
+
+import java.util.Scanner;
+
+public class PecahUang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\n==============================");
+        System.out.println("CEK PECAHAN UANG");
+        System.out.println("==============================\n");
+
+        System.out.print("Masukkan uang\t: ");
+        int uangIn = input.nextInt();
+        input.close();
+
+        int lembar = 0;
+        int[] pecah = {100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100};
+        boolean next = false;
+
+        int i = 0;
+        while(!next){
+            if (uangIn >= pecah[i]) {
+                lembar = uangIn/pecah[i];
+                uangIn %= pecah[i];
+                System.out.println("Pecahan " + String.valueOf(pecah[i]) + "\t: " + lembar);
+                
+            } else if(uangIn < 100){
+                next = true;
+            }
+            i++;
+        }
+        System.out.println("\n==============================\n");
+        
+    }
+}
