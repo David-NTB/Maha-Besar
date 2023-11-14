98c644e41d0d9fbb07990d4d6e8e44b73202b976
David NTB <davidntb7000@gmail.com>
2023-11-14 18:26:14 +0700

    latihan konversi biner

---
+++ src/semester1/Latihan3/Biner.java
@@ -0,0 +1,40 @@
+package semester1.Latihan3;
+
+import java.util.Scanner;
+
+class Biner{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //menu
+        System.out.println("==============================");
+        System.out.println("KONVERSI BILANGAN KE BINER");
+        System.out.println("==============================");
+
+        //user input
+        System.out.print("Ubah biner : ");
+        int in = input.nextInt();
+        input.close();
+
+        //cek panjang biner
+        int index = 0;
+        int cekIn = in;
+        while(cekIn > 1){
+            cekIn /= 2;
+            index++;
+        }
+        
+        //mengkonversi ke biner
+        int[] biner = new int[index + 1];
+        for(int i = index; i >= 0; i--){
+            biner[i] = in % 2;
+            in /= 2;
+        }
+
+        //print ke layar
+        System.out.print("Biner : ");
+        for(int i = 0; i <= index; i++) System.out.print(biner[i]); 
+
+        System.out.println("\n==============================");
+    }
+}
---
+++ src/semester1/Latihan3/Prima.java
@@ -0,0 +1,5 @@
+package semester1.Latihan3;
+
+public class Prima {
+    
+}
