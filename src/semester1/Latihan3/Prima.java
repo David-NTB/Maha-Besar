04210ccc205fdecc5153d54a041949f96f082e98
David NTB <davidntb7000@gmail.com>
2023-11-14 19:28:23 +0700

    membuat cek prima

--- src/semester1/Latihan3/Prima.java
+++ src/semester1/Latihan3/Prima.java
@@ -1,5 +1,41 @@
 package semester1.Latihan3;
 
+import java.util.Scanner;
+
 public class Prima {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //menu
+        System.out.println("==============================");
+        System.out.println("MENGECEK BILANGAN PRIMA");
+        System.out.println("==============================");
+
+        //user input
+        System.out.print("Masukkan input : ");
+        int in = input.nextInt();
+        input.close();
+        
+        //membuat variabel pengecekan
+        int sisa = 1;
+        int[] cekPrima = new int[in];
+        for(int i = 0; i < (in-2); i++) {
+            cekPrima[i] = i + 2;
+
+            //melakukan pengecekan
+            while(sisa > 0){
+                sisa = in % cekPrima[i];
+                break;
+            }
+        }
+
+        //print ke layar
+        if(sisa == 0){
+            System.out.println(in + " bukan bilangan prima");
+        } else {
+            System.out.println(in + " adalah bilangan prima");
+        }
+
+        System.out.println("==============================");
+    }
 }
