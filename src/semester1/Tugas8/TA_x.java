d709454c5cee3e55c551193f66e5954eb9853c89
David NTB <davidntb7000@gmail.com>
2023-11-22 20:40:28 +0700

    Perbaikan TA_x

--- src/semester1/Tugas8/TA_x.java
+++ src/semester1/Tugas8/TA_x.java
@@ -11,7 +11,7 @@ public class TA_x {
         input.close();
 
         System.out.println("\n");
-        
+                
         //Looping FOR
         System.out.println("X Pake FOR");
         xPakeFor(size);
@@ -33,73 +33,27 @@ public class TA_x {
         int j = 1;
 
         do {
-            if (i <= size) {
-                j = 1;
-                do {
-                    if (j < i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j <= size) {
-                        System.out.print("    ");
-                    } else if (j == size + 1) {
-                        System.out.print(" *");
-                    }
-                    j++;
-                } while (j <= size * 2);
-            } else if (i > size) {
-                j = size * 2;
-                do {
-                    if (j > i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j > size) {
-                        System.out.print("    ");
-                    } else if (j == size - 1) {
-                        System.out.print(" *");
-                    }
-                    j--;
-                } while (j >= 1);
-            }
+            j=1;
+            do {
+                if((j==i)||(j == size - (i-1))) System.out.print(" *");
+                else System.out.print("  ");
+                j++;
+            } while(j <= size);
             System.out.println();
             i++;
-        } while (i <= size * 2);
+        } while(i <= size);
     }
 
     static void xPakeWhile(int size) {
         int i = 1;
         int j = 1;
 
-        while (i <= size * 2) {
-            if (i <= size) {
-                j = 1;
-                while (j <= size * 2) {
-                    if (j < i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j <= size) {
-                        System.out.print("    ");
-                    } else if (j == size + 1) {
-                        System.out.print(" *");
-                    }
-                    j++;
-                }
-            } else if (i > size) {
-                j = size * 2;
-                while (j >= 1) {
-                    if (j > i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j > size) {
-                        System.out.print("    ");
-                    } else if (j == size - 1) {
-                        System.out.print(" *");
-                    }
-                    j--;
-                }
+        while(i <= size) {
+            j=1;
+            while(j <= size) {
+                if((j==i)||(j == size - (i-1))) System.out.print(" *");
+                else System.out.print("  ");
+                j++;
             }
             System.out.println();
             i++;
@@ -107,31 +61,10 @@ public class TA_x {
     }
 
     static void xPakeFor(int size) {
-        for (int i = 1; i <= size * 2; i++) {
-            if (i <= size) {
-                for (int j = 1; j <= size * 2; j++) {
-                    if (j < i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j <= size) {
-                        System.out.print("    ");
-                    } else if (j == size + 1) {
-                        System.out.print(" *");
-                    }
-                }
-            } else if (i > size) {
-                for (int j = size * 2; j >= 1; j--) {
-                    if (j > i) {
-                        System.out.print("  ");
-                    } else if (j == i) {
-                        System.out.print(" *");
-                    } else if (j > size) {
-                        System.out.print("    ");
-                    } else if (j == size - 1) {
-                        System.out.print(" *");
-                    }
-                }
+        for(int i = 1; i <= size; i++) {
+            for(int j = 1; j <= size; j++) {
+                if((j==i)||(j == size - (i-1))) System.out.print(" *");
+                else System.out.print("  ");
             }
             System.out.println();
         }
