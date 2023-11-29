89878dc9b685f75f1da0b71befc5dd47b365a525
David NTB <davidntb7000@gmail.com>
2023-11-29 22:08:09 +0700

    menyelesaikan TA4

--- src/semester1/Tugas9/TA4.java
+++ src/semester1/Tugas9/TA4.java
@@ -1,5 +1,33 @@
 package semester1.Tugas9;
 
-public class TA4 {
-    
+import java.util.ArrayList;
+import java.util.Random;
+
+class TA4 {
+    ArrayList<Integer> arrNilai = new ArrayList<Integer>(); 
+    Random getRandom = new Random();
+
+    public static void main(String[] args) {
+        TA4 dn = new TA4();
+        dn.runThis();
+    }
+
+    void runThis() {
+        isiNilai();
+        tampilArray();
+    }
+
+    void isiNilai() {
+        // System.out.println("\n\nPengisian Nilai");
+        for (int a = 0; a <= 10; a++) {
+            arrNilai.add(getRandom.nextInt(102));
+        }
+    }
+
+    void tampilArray() {
+        System.out.println("\n\nDaftar Nilai");
+        for (int a = 0; a < arrNilai.size(); a++) {
+            System.out.println("Index " + a + "\t: " + arrNilai.get(a) + " ");
+        }
+    }
 }
