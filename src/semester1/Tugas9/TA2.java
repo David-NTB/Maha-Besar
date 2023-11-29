9b7595eee10a47a478d0d2eac293e0e271fb2824
David NTB <davidntb7000@gmail.com>
2023-11-29 21:40:17 +0700

    menambahkan range

--- src/semester1/Tugas9/TA2.java
+++ src/semester1/Tugas9/TA2.java
@@ -15,12 +15,10 @@ public class TA2 {
     private ArrayList<String> NIM = new ArrayList<String>();
     private ArrayList<Double> grade = new ArrayList<Double>();
 
-
     public static void main(String[] args) {
         TA2 mk = new TA2();
         mk.menu();
     }
-    
 
     private void menu() {
         boolean next = true;
@@ -67,25 +65,41 @@ public class TA2 {
         System.out.println("==============================");
         System.out.println("INPUT DATA NILAI");
         System.out.println("==============================");
-        
+
         System.out.print("Nama Lengkap \t: ");
         name.add(input.nextLine());
         System.out.print("NIM \t\t: ");
         NIM.add(input.nextLine());
 
-        System.out.print("\nNilai Assessment 1 : ");
-        this.ass1 = input.nextInt();
-        System.out.print("Nilai Assessment 2 : ");
-        this.ass2 = input.nextInt();
-        System.out.print("Nilai Assessment 3 : ");
-        this.ass3 = input.nextInt();
-        System.out.print("Nilai Tugas\t   : ");
-        this.tugas = input.nextInt();
-        System.out.print("Nilai Praktikum\t   : ");
-        this.praktikum = input.nextInt();
-        
+        boolean next = true;
+        do {
+            System.out.print("\nNilai Assessment 1 : ");
+            this.ass1 = input.nextInt();
+            System.out.print("Nilai Assessment 2 : ");
+            this.ass2 = input.nextInt();
+            System.out.print("Nilai Assessment 3 : ");
+            this.ass3 = input.nextInt();
+            System.out.print("Nilai Tugas\t   : ");
+            this.tugas = input.nextInt();
+            System.out.print("Nilai Praktikum\t   : ");
+            this.praktikum = input.nextInt();
+
+            if (cek(this.ass1) && cek(this.ass2) && cek(this.ass3) && cek(this.tugas) && cek(this.praktikum))
+                next = false;
+            else
+                System.err.println("\nError..!! Rentang nilai harus 0-100");;
+
+        } while (next);
+
         processData();
         System.out.println("\nNilai sudah diproses . . .");
+    }
+
+    private boolean cek(double input) {
+        if ((input >= 0) && (input <= 100))
+            return true;
+        else
+            return false;
     }
 
     private void processData() {
@@ -94,7 +108,7 @@ public class TA2 {
         this.ass3 *= 10;
         this.tugas *= 20;
         this.praktikum *= 40;
-        double total = (this.ass1 + this.ass2 + this.ass3 + this.tugas + this.praktikum)/100;
+        double total = (this.ass1 + this.ass2 + this.ass3 + this.tugas + this.praktikum) / 100;
         grade.add(total);
     }
 
@@ -102,8 +116,8 @@ public class TA2 {
         System.out.println("==============================");
         System.out.println("MENAMPILKAN SEMUA DATA");
         System.out.println("==============================");
-        
-        for(int i = 0; i < name.size(); i++) {
+
+        for (int i = 0; i < name.size(); i++) {
             System.out.println("\nNama Lengkap\t: " + name.get(i));
             System.out.println("NIM\t\t: " + NIM.get(i));
             System.out.println("Nilai Akhir\t: " + grade.get(i));
@@ -112,15 +126,15 @@ public class TA2 {
 
     private void next() {
         System.out.println("==============================");
-            System.out.println("Tekan ENTER untuk lanjut . . .");
-            input.nextLine();
-            input.nextLine();
+        System.out.println("Tekan ENTER untuk lanjut . . .");
+        input.nextLine();
+        input.nextLine();
     }
 
     private void cls() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            
+
         } catch (Exception e) {
             System.err.println("Error : " + e);
         }
--- src/semester1/Tugas9/TA3.java
+++ src/semester1/Tugas9/TA3.java
@@ -1,5 +1,32 @@
 package semester1.Tugas9;
 
+import java.util.Scanner;
+
 public class TA3 {
-    
+
+    int arr[] = new int[10];
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        TA3 ca = new TA3();
+        ca.inputData();
+        ca.viewData();
+    }
+
+    public void inputData() {
+        System.out.println("Input Data");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Nilai " + (i + 1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println();
+    }
+
+    public void viewData() {
+        System.out.println("View Data");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai " + (i + 1) + " : " + arr[i]);
+        }
+        System.out.println();
+    }
 }
